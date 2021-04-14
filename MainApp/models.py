29fda0b37099d150f4f8acb9852eb4d0b714aa45
Chris Keller <ckeller191@gmail.com>
2021-04-14 15:48:00 -0500

    end of class 4-14

--- MainApp/models.py
+++ MainApp/models.py
@@ -22,5 +22,6 @@ class Entry(models.Model):
 
     def __str__(self):
         return f"{self.text[:50]}..."
+        
 
 
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    ''' The home page for learning log. '''
+    return render(request, 'MainApp/index.html')
+
