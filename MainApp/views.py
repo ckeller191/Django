b670ed3326db6e099fffc45d27fd41fbec02150d
Chris Keller <ckeller191@gmail.com>
2021-04-19 15:46:07 -0500

    end of class 4/19

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 
 def index(request):
     ''' The home page for learning log. '''
     return render(request, 'MainApp/index.html')
+
+def topics(request):
+    topics = Topic.objects.order_by('date_added') # put a - in front of the variable to make it descending order
+
+
+    context = {'topics':topics}
+    return render(request, 'MainApp/topics.html', context)
+
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id = topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+
+    context = {'topic':topic, 'entries':entries}
+
+    return render(request, 'MainApp.topic/html', context)
+
+
+def new_topic(request):
+
 
